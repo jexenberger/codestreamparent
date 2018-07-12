0240daafd31aa3e3268eb1d51663e5c05a8df2d3
ABJE268 <julian.exenberger@absa.co.za>
2018-07-12 17:25:46 +0200

    Added:  - Worked on versioning and Help commandlet

--- codestream-core/src/main/kotlin/io/codestream/core/runtime/TaskRegistry.kt
+++ codestream-core/src/main/kotlin/io/codestream/core/runtime/TaskRegistry.kt
@@ -10,11 +10,11 @@ import io.codestream.core.api.TaskType
 object TaskRegistry {
 
     fun resolve(type: TaskType, callingModule: CodestreamModule? = null) : TaskDescriptor? {
-        val version =  callingModule?.getDependencyVersion(type.moduleName)
-        val module = version?.let { ModuleRegistry[ModuleId(type.moduleName, version)] }
-                ?: ModuleRegistry.getLatestVersion(type.moduleName)
+        val version = callingModule?.getDependencyVersion(type.moduleName)
+        val targetVersion = version?.let { it } ?: type.module.version
+        val module = ModuleRegistry[ModuleId(type.moduleName, targetVersion)]
         val resolvedModule = module ?: throw ModuleDoesNotExistException("${type.module}@${CodestreamModule.versionString(version ?: defaultVersion)}")
-        return resolvedModule[type]
+        return resolvedModule.getByName(type.name)
     }
 
 }
